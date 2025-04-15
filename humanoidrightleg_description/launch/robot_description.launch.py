60b805c2bc2ef630dd73756fb83e8440de749d23
newtonjeri <newtonkaris45@gmail.com>
2025-04-15 21:25:47 +0300

    Refactor launch file to streamline robot description setup and improve readability

--- humanoidrightleg_description/launch/robot_description.launch.py
+++ humanoidrightleg_description/launch/robot_description.launch.py
@@ -1,35 +1,58 @@
-#!/usr/bin/env python3
-
+import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command, LaunchConfiguration
+
+package_name = "humanoidrightleg_description"
+xacro_file = "humanoidrightleg.xacro"
 
 def generate_launch_description():
 
+    urdf_file = os.path.join(get_package_share_directory(package_name), "urdf", xacro_file)
 
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("humanoidrightleg_description"),
-                    "urdf",
-                    "humanoidrightleg.xacro",
-                ]
-            ),
-
-        ]
+    model_arg = DeclareLaunchArgument(
+        name = "model",
+        default_value = urdf_file,
+        description = "Absolute path to robot urdf"
     )
-    robot_description = {"robot_description": robot_description_content}
+
+    robot_description = ParameterValue(Command(
+            ['xacro ', LaunchConfiguration("model")]
+        )
+    )
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": robot_description}],
+        arguments=[urdf_file],
+    )
+
+    joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        output="screen",
+    )
+
+    rviz_config = os.path.join(get_package_share_directory(package_name), "rviz", "humanoidrightleg.rviz")
+
+    rviz2 = Node(
+        package="rviz2",
+        executable = "rviz2",
+        name = "rviz2",
+        output = "screen",
+        arguments=["-d", rviz_config]
+    )
+
 
     return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[robot_description]),
+        model_arg,
+        robot_state_publisher,
+        joint_state_publisher_gui,
+        rviz2,
     ])
